20375600ab74a29ecba1d34bf0287a9307253104
matthewjchou <matthew.chouj@gmail.com>
2020-05-23 17:56:01 -0400

    test, moved go files to follow convention

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/url"
+	"utils"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(utils.GetWords())
+}
---
+++ utils/words.go
@@ -0,0 +1,5 @@
+package utils
+
+func GetWords() string {
+	return "Hello World"
+}
