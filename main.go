246ba5352046d586b265f99b42f0f9d24598c53f
matthewjchou <matthew.chouj@gmail.com>
2020-05-24 02:01:11 -0400

    Gather 5000 of the most common words, return an N size subset

--- main.go
+++ main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
-	"encoding/json"
+	"type_tracker/utils"
+	"github.com/gotk3/gotk3/gtk"
+	// "encoding/json"
 	"log"
-	"net/http"
-	"net/url"
-	"utils"
+	// "net/http"
+	// "net/url"
 	"fmt"
 )
 
+func start() {
+	fmt.Println("HERE")
+	gtk.Init(nil)
+	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	win.SetTitle("type_tracker")
+	win.Connewct("destroy", func() {
+		gtk.MainQuit()
+	})
+
+	win.SetDefaultSize(800, 600)
+	win.showAll()
+	gtk.Main()
+}
+
+
 func main() {
-	fmt.Println(utils.GetWords())
+	words := utils.GetNWords(300)
+	start()
 }
--- utils/words.go
+++ utils/words.go
@@ -1,5 +1,52 @@
 package utils
 
-func GetWords() string {
-	return "Hello World"
+import (
+	"bufio"
+	"os"
+	"log"
+	"math/rand"
+)
+
+var words *[]string
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func getAllWords() *[]string {
+	f, err := os.Open("resources/commonWords.txt")
+	check(err)
+	defer func () {
+		err = f.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}() // defer requires a function call, this defines an anonymous function and calls it
+	words := make([]string, 0)
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		words = append(words, s.Text()) //append is funky, remember this!
+	}
+	return &words
+}
+
+func randomShuffle(N uint) *[]string {
+	var words_set = make([]string, len(*words))
+	copy(words_set, *words)
+	//rand.Shuffle takes a swap function as the second parameter
+	rand.Shuffle(len(words_set), func(i int, j int) {words_set[i], words_set[j] = words_set[j], words_set[i]})
+	words_set = words_set[:N]
+	return &words_set
+}
+
+func GetNWords(N uint) *[]string {
+	if words == nil {
+		//if first time running, get all the words
+		words = getAllWords()
+	}
+	w := randomShuffle(N)
+
+	return w
 }
